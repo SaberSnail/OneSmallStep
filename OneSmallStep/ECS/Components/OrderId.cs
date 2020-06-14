b90ca24fe57fd57298396e144d8829bfa3389849
abatishko <andrew@batishko.net>
2020-06-14 08:12:37 -0700

    Reformat code

--- OneSmallStep/ECS/Components/OrderId.cs
+++ OneSmallStep/ECS/Components/OrderId.cs
@@ -6,40 +6,19 @@ namespace OneSmallStep.ECS.Components
 	[DebuggerDisplay("{m_id}")]
 	public struct OrderId : IEquatable<OrderId>
 	{
-		public OrderId(uint id)
-		{
-			m_id = id;
-		}
+		public OrderId(uint id) => m_id = id;
 
-		public override int GetHashCode()
-		{
-			return m_id.GetHashCode();
-		}
+		public override int GetHashCode() => m_id.GetHashCode();
 
-		public override bool Equals(object that)
-		{
-			return that is OrderId id && Equals(id);
-		}
+		public override bool Equals(object that) => that is OrderId id && Equals(id);
 
-		public bool Equals(OrderId that)
-		{
-			return m_id == that.m_id;
-		}
+		public bool Equals(OrderId that) => m_id == that.m_id;
 
-		public static bool operator ==(OrderId left, OrderId right)
-		{
-			return left.Equals(right);
-		}
+		public static bool operator ==(OrderId left, OrderId right) => left.Equals(right);
 
-		public static bool operator !=(OrderId left, OrderId right)
-		{
-			return !left.Equals(right);
-		}
+		public static bool operator !=(OrderId left, OrderId right) => !left.Equals(right);
 
-		public override string ToString()
-		{
-			return m_id.ToString();
-		}
+		public override string ToString() => m_id.ToString();
 
 		readonly uint m_id;
 	}
